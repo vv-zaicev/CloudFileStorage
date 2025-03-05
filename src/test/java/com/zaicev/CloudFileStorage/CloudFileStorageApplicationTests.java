f24d01be04edb4f7dbe6e9f747277b3b13ba50a3
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-05 20:41:35 +0600

    создание проекта

---
+++ src/test/java/com/zaicev/CloudFileStorage/CloudFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaicev.CloudFileStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
