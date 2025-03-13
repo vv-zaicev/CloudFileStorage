4a4c44f230a7bc37b1c937d24dae7d1db2bc8da6
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-13 22:45:48 +0600

    созданы dto

---
+++ src/main/java/com/zaicev/CloudFileStorage/DTO/StorageObjectDTO.java
@@ -0,0 +1,17 @@
+package com.zaicev.CloudFileStorage.DTO;
+
+import com.zaicev.CloudFileStorage.storage.models.StorageObjectType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StorageObjectDTO {
+	private String name;
+    private String path;
+    private int size;
+    private StorageObjectType type;
+}
