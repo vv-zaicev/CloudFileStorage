873200fd9817eb0cdc26ceec73566fbb5fbeaa0a
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-17 21:29:14 +0600

    добавлена анотация сервиса

--- src/main/java/com/zaicev/CloudFileStorage/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/zaicev/CloudFileStorage/security/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.zaicev.CloudFileStorage.security.models.User;
 import com.zaicev.CloudFileStorage.security.models.UserDetailsImpl;
 import com.zaicev.CloudFileStorage.security.repository.UserRepository;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 	
 	@Autowired
