92bfb8c916da29d52fdc99fc3b444d06ead76c20
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-25 21:21:53 +0600

    добавлена конфигурация для mimio

---
+++ src/main/java/com/zaicev/CloudFileStorage/config/DataConfig.java
@@ -0,0 +1,25 @@
+package com.zaicev.CloudFileStorage.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+
+@Configuration
+public class DataConfig {
+	@Value("${MINIO_SECRET_KEY}")
+	private String secretToken;
+
+	@Value("${MINIO_ACCESS_KEY}")
+	private String accessToken;
+
+	@Bean
+	MinioClient minioClient() {
+		return MinioClient
+				.builder()
+				.endpoint("localhost", 9000, false)
+				.credentials(accessToken, secretToken)
+				.build();
+	}
+}
