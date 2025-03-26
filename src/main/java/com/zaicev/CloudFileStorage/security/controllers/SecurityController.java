a76b468b16ba5f6b6e82f53e50f04d6b1856843d
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-26 20:46:52 +0600

    добавлен контроллер для определения пользователя

--- src/main/java/com/zaicev/CloudFileStorage/security/controllers/SecurityController.java
+++ src/main/java/com/zaicev/CloudFileStorage/security/controllers/SecurityController.java
@@ -2,6 +2,7 @@ package com.zaicev.CloudFileStorage.security.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,5 @@ public class SecurityController {
 		userDTO.setPassword(null);
 		return userDTO;
 	}
-	
-	
 
 }
---
+++ src/main/java/com/zaicev/CloudFileStorage/security/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.zaicev.CloudFileStorage.security.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zaicev.CloudFileStorage.dto.UserDTO;
+import com.zaicev.CloudFileStorage.security.models.UserDetailsImpl;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	@GetMapping("/me")
+	public UserDTO getCurrentUser(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setUsername(userDetailsImpl.getUsername());
+		return userDTO;	
+	}
+}
