332f98135ebe1e6c7a9adf73f3d32425f79eeb83
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-03-30 20:53:34 +0600

    реализованны основные методы

--- src/main/java/com/zaicev/CloudFileStorage/storage/repository/MinIORepository.java
+++ src/main/java/com/zaicev/CloudFileStorage/storage/repository/MinIORepository.java
@@ -1,26 +1,119 @@
 package com.zaicev.CloudFileStorage.storage.repository;
 
+import java.io.InputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.zaicev.CloudFileStorage.storage.exception.StorageObjectNotFound;
+
+import io.minio.CopyObjectArgs;
+import io.minio.CopySource;
+import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.StatObjectArgs;
+import io.minio.StatObjectResponse;
+import io.minio.errors.ErrorResponseException;
 
 @Repository
 public class MinIORepository {
-	
+
 	@Autowired
 	private MinioClient minioClient;
-	
-	private static final String USER_BUCKET_NAME="user-files";
-	
-	public void downloadFile(String path, int userId, MultipartFile file) throws Exception {
-		String fullPath = String.format("/user-%d-files/%s%s", userId, path, file.getOriginalFilename());
-		minioClient.putObject(PutObjectArgs.builder()
-				.bucket(USER_BUCKET_NAME)
-				.object(fullPath)
-				.stream(file.getInputStream(), file.getSize(), -1)
-				.build());
+
+	private static final String USER_BUCKET_NAME = "user-files";
+
+	public void uploadFile(String path, MultipartFile file) {
+		try {
+			minioClient
+					.putObject(PutObjectArgs
+							.builder()
+							.bucket(USER_BUCKET_NAME)
+							.object(path)
+							.stream(file.getInputStream(), file.getSize(), -1)
+							.build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	public long getObjectSize(String path) throws Exception {
+		try {
+			StatObjectResponse stat = minioClient
+					.statObject(StatObjectArgs
+							.builder()
+							.bucket(USER_BUCKET_NAME)
+							.object(path)
+							.build());
+			return stat.size();
+		} catch (ErrorResponseException e) {
+			if (e.errorResponse().code().equals("NoSuchKey")) {
+				throw new StorageObjectNotFound(path);
+			}
+			throw e;
+		}
+	}
+
+	public InputStream getObject(String path) throws Exception {
+		try {
+			InputStream stream = minioClient
+					.getObject(GetObjectArgs
+							.builder()
+							.bucket(USER_BUCKET_NAME)
+							.object(path)
+							.build());
+			return stream;
+		} catch (ErrorResponseException e) {
+			if (e.errorResponse().code().equals("NoSuchKey")) {
+				throw new StorageObjectNotFound(path);
+			}
+			throw e;
+		}
+	}
+
+	public void deleteObject(String path) throws Exception {
+		try {
+			minioClient
+					.removeObject(RemoveObjectArgs
+							.builder()
+							.bucket(USER_BUCKET_NAME)
+							.object(path)
+							.build());
+		} catch (ErrorResponseException e) {
+			if (e.errorResponse().code().equals("NoSuchKey")) {
+				throw new StorageObjectNotFound(path);
+			}
+			throw e;
+		}
+	}
+
+	public void copyObject(String sourcePath, String copyPath) throws Exception {
+		try {
+			minioClient
+					.copyObject(CopyObjectArgs
+							.builder()
+							.bucket(USER_BUCKET_NAME)
+							.object(copyPath)
+							.source(CopySource
+									.builder()
+									.bucket(USER_BUCKET_NAME)
+									.object(sourcePath)
+									.build())
+							.build());
+		} catch (ErrorResponseException e) {
+			if (e.errorResponse().code().equals("NoSuchKey")) {
+				throw new StorageObjectNotFound(sourcePath);
+			}
+			throw e;
+		}
+	}
+	
+	public void moveObject(String sourcePath, String targetPath) throws Exception{
+		copyObject(sourcePath, targetPath);
+		deleteObject(sourcePath);
+	}
+
 }
