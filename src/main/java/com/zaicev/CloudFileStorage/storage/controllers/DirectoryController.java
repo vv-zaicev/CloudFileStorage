2f7bfa6fe924d064e1cbe5ebe5be8ff179e58aaf
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-04-11 21:01:37 +0600

    добавлены обработчики ошибок

--- src/main/java/com/zaicev/CloudFileStorage/storage/controllers/DirectoryController.java
+++ src/main/java/com/zaicev/CloudFileStorage/storage/controllers/DirectoryController.java
@@ -1,15 +1,21 @@
 package com.zaicev.CloudFileStorage.storage.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.zaicev.CloudFileStorage.security.models.UserDetailsImpl;
+import com.zaicev.CloudFileStorage.storage.exception.StorageObjectExist;
+import com.zaicev.CloudFileStorage.storage.exception.StorageObjectNotFound;
 import com.zaicev.CloudFileStorage.storage.models.StorageObject;
 import com.zaicev.CloudFileStorage.storage.services.DirectoryService;
 import com.zaicev.CloudFileStorage.storage.services.PathService;
@@ -26,11 +32,25 @@ public class DirectoryController {
 	@GetMapping()
 	public List<StorageObject> getAllFromDirectory(String path, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl)
 			throws Exception {
-		return directoryService.getAllFromPath(pathService.getFullPath(path, userDetailsImpl.getId()));
+		List<StorageObject> storageObjects = new ArrayList<>();
+		try {
+			 storageObjects = directoryService.getAllFromPath(pathService.getFullPath(path, userDetailsImpl.getId()));
+		} catch (StorageObjectNotFound e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, path);
+		}
+		
+		return storageObjects;
+		
 	}
 
 	@PostMapping()
 	public StorageObject createFolder(String path, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) throws Exception {
-		return directoryService.createFolder(pathService.getFullPath(path, userDetailsImpl.getId()));
+		StorageObject storageObject;
+		try {
+			storageObject = directoryService.createFolder(pathService.getFullPath(path, userDetailsImpl.getId()))
+		} catch (StorageObjectExist e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT, path);
+		}
+		return storageObject;
 	}
 }
