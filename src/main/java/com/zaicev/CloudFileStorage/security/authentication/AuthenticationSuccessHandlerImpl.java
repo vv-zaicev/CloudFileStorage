7d4539022f0abef603c9d6b1fe3e83e2d7bbc9cf
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-04-15 21:01:41 +0600

    добавлено создание сессии

--- src/main/java/com/zaicev/CloudFileStorage/security/authentication/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/zaicev/CloudFileStorage/security/authentication/AuthenticationSuccessHandlerImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Collections;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
 
@@ -19,6 +21,11 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
 			throws IOException, ServletException {
+		HttpSession session = request.getSession(true);
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+
 		response.setStatus(HttpServletResponse.SC_OK);
 		response.setContentType("application/json");
 
