e6ba13e7d1a8da79f0ac194880bb2b05305e725a
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-04-20 18:59:17 +0600

    добавлена авторизация после регистрации

--- src/main/java/com/zaicev/CloudFileStorage/security/controllers/SecurityController.java
+++ src/main/java/com/zaicev/CloudFileStorage/security/controllers/SecurityController.java
@@ -2,34 +2,44 @@ package com.zaicev.CloudFileStorage.security.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.zaicev.CloudFileStorage.dto.UserDTO;
+import com.zaicev.CloudFileStorage.security.exceptions.UsernameAlreadyTakenException;
 import com.zaicev.CloudFileStorage.security.services.AuthService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class SecurityController {
-	
+
 	@Autowired
 	private AuthService authService;
-	
+
 	@PostMapping("/sign-up")
 	@ResponseStatus(HttpStatus.CREATED)
-	public UserDTO signup(@RequestBody UserDTO userDTO)  {
-		authService.registerUser(userDTO);
-		userDTO.setPassword(null);
-		return userDTO;
+	public UserDTO signup(@RequestBody UserDTO userDTO, HttpServletRequest httpServletRequest) throws Exception {
+		try {
+			authService.registerUser(userDTO, httpServletRequest);
+			userDTO.setPassword(null);
+			return userDTO;
+		} catch (UsernameAlreadyTakenException e) {
+			log.warn("username taked");
+			throw new ResponseStatusException(HttpStatus.CONFLICT);
+		}
+
 	}
-	
+
 	@PostMapping("/sign-in")
-	public UserDTO signin(@RequestBody UserDTO userDTO){
+	public UserDTO signin(@RequestBody UserDTO userDTO) {
 		userDTO.setPassword(null);
 		return userDTO;
 	}
--- src/main/java/com/zaicev/CloudFileStorage/security/services/AuthService.java
+++ src/main/java/com/zaicev/CloudFileStorage/security/services/AuthService.java
@@ -1,32 +1,58 @@
 package com.zaicev.CloudFileStorage.security.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.zaicev.CloudFileStorage.dto.UserDTO;
+import com.zaicev.CloudFileStorage.security.exceptions.UsernameAlreadyTakenException;
 import com.zaicev.CloudFileStorage.security.models.User;
 import com.zaicev.CloudFileStorage.security.repository.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Service
 public class AuthService {
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-	
-	public void registerUser(UserDTO userDTO) {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	public void registerUser(UserDTO userDTO, HttpServletRequest httpRequest) {
+		createUser(userDTO);
+		loginUser(userDTO, httpRequest);
+	}
+
+	private void createUser(UserDTO userDTO) {
 		if (userRepository.existsByUsername(userDTO.getUsername())) {
-			throw new RuntimeException("Username is already taken");
+			throw new UsernameAlreadyTakenException(userDTO.getUsername());
 		}
-		
+
 		User user = new User();
 		user.setUsername(userDTO.getUsername());
 		user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-		
+
 		userRepository.save(user);
 	}
-	
-	
+
+	private void loginUser(UserDTO userDTO, HttpServletRequest httpRequest) {
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(userDTO.getUsername(), userDTO.getPassword()));
+
+		SecurityContext context = SecurityContextHolder.getContext();
+		context.setAuthentication(authentication);
+		HttpSession session = httpRequest.getSession(true);
+		session.setAttribute("SPRING_SECURITY_CONTEXT", context);
+	}
+
 }
