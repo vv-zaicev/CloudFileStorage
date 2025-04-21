fdb330899471c622f28b91c8316a4f926dbd321f
Vadim Zaicev <zaicevvadim9@gmail.com>
2025-04-21 21:39:08 +0600

    дто для пользователя разделен для запроса и ответа

--- src/main/java/com/zaicev/CloudFileStorage/dto/UserRequestDTO.java
+++ src/main/java/com/zaicev/CloudFileStorage/dto/UserRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDTO {
+public class UserRequestDTO {
 	private String username;
 	private String password;
 }
---
+++ src/main/java/com/zaicev/CloudFileStorage/dto/UserResponseDTO.java
@@ -0,0 +1,10 @@
+package com.zaicev.CloudFileStorage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserResponseDTO {
+	private String username;
+}
